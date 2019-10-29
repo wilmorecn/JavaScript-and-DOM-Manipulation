987cd7da6d741cc7bba48af5244ef777fd7c3ccb
Wilmorecn <wilmorecn@gmail.com>
2019-10-28 22:05:46 -0500

    commit 2

--- StarterCode/static/js/app-optional.js
+++ StarterCode/static/js/app-optional.js
@@ -20,10 +20,33 @@ button.on("click", function () {
     var filteredData = tableData
     // Select the input element and get the raw HTML node
     var inputDate = d3.select("#datetime");
-    // Get the value property of the input element for Date
     var inputValueDate = inputDate.property("value");
     if (inputValueDate) {
         filteredData = filteredData.filter(tableData => tableData.datetime === inputValueDate)
+    };
+
+    var inputcity = d3.select("#city");
+    var inputValuecity = inputcity.property("value");
+    if (inputValuecity) {
+        filteredData = filteredData.filter(tableData => tableData.city === inputValuecity)
+    };
+
+    var inputstate = d3.select("#state");
+    var inputValuestate = inputstate.property("value");
+    if (inputValuestate) {
+        filteredData = filteredData.filter(tableData => tableData.state === inputValuestate)
+    };
+
+    var inputcountry = d3.select("#country");
+    var inputValuecountry = inputcountry.property("value");
+    if (inputValuecountry) {
+        filteredData = filteredData.filter(tableData => tableData.country === inputValuecountry)
+    };
+
+    var inputshape = d3.select("#shape");
+    var inputValueshape = inputshape.property("value");
+    if (inputValueshape) {
+        filteredData = filteredData.filter(tableData => tableData.shape === inputValueshape)
     };
     //Render Table
     // Get a reference to the table body
